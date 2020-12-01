60c6aacabe5dd10376747bde83f8df1c7450eabf
19Neveragain <talesbuzanello@gmail.com>
2020-12-01 12:16:05 -0300

    bug2

--- src/database/test.js
+++ src/database/test.js
@@ -32,7 +32,37 @@ Database.then(async (db) => {
     }
   ]
 
-  await createProffy(db, { proffyValue, classValue, classScheduleValues })
-}).catch(() => {
-  console.log("asddsa")
+  // await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+  // consultar dados inseridos
+
+  // todos os proffys
+  const selectedProffys = await db.all("SELECT * FROM proffys")
+  // console.log(selectedProffys)
+
+  // consultar as classes de um determinado professor
+  // trazer junto os dados do professor
+
+  const selectClassesandProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+  
+  `)
+  //console.log(selectClassesandProffys)
+
+  // o horario que a pessoa trabalha, por exemplo, é das 8h - 18h
+  // o horario do time_from precisa ser menor ou igual ao horario solicitado
+  // o time_to precisa ser acima
+  const selectClassesSchedules = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = 1
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "1300"
+    AND class_schedule.time_to > "1300"
+  `)
+
+  //console.log(selectClassesSchedules)
 })
